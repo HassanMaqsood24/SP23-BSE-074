c48bbc193f5feb18e6d68b4de7d929b0409682be
HassanMaqsood24 <hmaqsood033@gmail.com>
2024-12-17 14:20:03 -0800

    Theory Assignment#3
    Theory Assignment#3

--- LAB Assignment # 3/Server.js
+++ LAB Assignment # 3/Server.js
@@ -3,14 +3,12 @@ let server = express();
 server.set("view engine", "ejs");
 server.use(express.static("public"));
 
-
 server.get("/Bootstrap", (req, res) => {
     return res.render("Bootstrap");
   });
   server.get("/Portfolio", (req, res) => {
     return res.send(res.render("Portfolio"));
   });
-
   server.listen(2002, () => {
     console.log(`Server Started at localhost:2002`);
   });
